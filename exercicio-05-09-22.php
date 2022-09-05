47826a1700ca0e3f6400edd372b2b6cb1756bff1
Ferneko <fcgraciano@gmail.com>
2022-09-05 14:07:20 -0300

    Exercicios de fixação

---
+++ exercicio-05-09-22.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+    <title>Exercícios de Fixação</title>
+    <link href="./estilo.css" rel="stylesheet" />
+</head>
+
+<body>
+    <p>
+        -)Crie uma variável e atribua um array com os seguintes itens : 20, 15, 30, 17, 8
+    </p>
+    <p>
+        -)Faça uma função que imprima o array do primeiro exercício.
+    </p>
+    <p>
+        -)Crie uma função que some todos os itens do array do exercício 1;
+    </p>
+    <p>
+        -)Utilizando o For imprima a tabuada do 144.
+    </p>
+    <p>
+        -)Exiba na tela todos os itens pares de 251 de 544.
+    </p>
+    <p>
+        -) Adicione 3 novos itens no array do exercício 1 usando a função rand(-10,10).
+    </p>
+    <p>
+        -)Faça uma função que receba a porcentagem e um numero e calcule a porcentagem desse numero.
+    </p>
+    <p>
+        -)Exiba na tela a quantidade de pares, impares, negativos e positivos do array do exercício 1
+    </p>
+    <p>
+        -)Calcule a média do array do exercício 1
+    </p>
+</body>
+
+</html>
